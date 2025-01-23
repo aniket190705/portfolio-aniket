251383b5807fed3e2c8fe1f339fd57add05f1e6a
aniket190705 <aniket190705@gmail.com>
2025-01-23 20:22:11 +0530

    finally

--- client/src/assets/Components/Footer.jsx
+++ client/src/assets/Components/Footer.jsx
@@ -19,13 +19,16 @@ const Footer = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+      const response = await fetch(
+        "https://portfolio-aniket-backend.onrender.com/send-email",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
       const result = await response.json();
       setSent(true);
       console.log(result.message, isSubmitting);
