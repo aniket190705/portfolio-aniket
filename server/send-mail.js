63e4f4b42fa7d2e6e896851dcc691230d04e46df
aniket190705 <aniket190705@gmail.com>
2025-01-23 19:32:40 +0530

    render

--- server/send-mail.js
+++ server/send-mail.js
@@ -5,6 +5,7 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import cors from "cors";
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
@@ -43,7 +44,7 @@ app.post("/send-email", async (req, res) => {
     }
 
 });
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
