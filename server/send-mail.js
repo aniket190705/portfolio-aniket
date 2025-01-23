86e2ae083929016e26e97a456b3db86a6673c18d
aniket190705 <aniket190705@gmail.com>
2025-01-23 12:22:40 +0530

    vercel.json

--- server/send-mail.js
+++ server/send-mail.js
@@ -1,6 +1,10 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const cors = require("cors");
+// const express = require("express");
+// const nodemailer = require("nodemailer");
+// const cors = require("cors");
+
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
@@ -36,3 +40,6 @@ app.post("/send-email", async (req, res) => {
 
 });
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
